754ec2d3c44592d899c45e42e9a2d9bf34f309b1
Sam <samara.amanda01@gmail.com>
2021-04-09 00:44:05 -0400

    commit tree 2c5097df4008cd290f34fd398269b9ca6720bbf0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(array,func){
+    let r = []
+    array.forEach(elem => {
+      r.push( func(elem) )
+    })
+    return r
+  }
+  
+  function reduce(array, func, start){
+    let r = !!start ? start : array[0]
+    let i = !!start ? 0 : 1
+  
+    for (i; i < array.length; i++){
+      r = func(array[i], r)
+    }
+  
+    return r
+  }
+  
